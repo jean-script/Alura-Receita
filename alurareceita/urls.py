a301b9cf4e9e519c96c0a1c8af987645b12abf15
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-09-27 19:38:33 -0300

    first commit

---
+++ alurareceita/urls.py
@@ -0,0 +1,12 @@
+
+from xml.dom.minidom import Document
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('receitas.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
